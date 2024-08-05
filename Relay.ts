5732fb118d57f4b28ebd6724787b7553fdde319e
Craig Albert <me@craigalbert.com>
2024-08-04 23:17:26 -0400

    improving Relay library so generic types aren't required.

--- Relay.ts
+++ Relay.ts
@@ -1,24 +1,24 @@
-export default class Relay<T> {
-    private onChangeCallbacks: Array<(data: T extends void | undefined | null ? T : Exclude<T, undefined>) => void> = [];
-    private promiseResolvers: Array<(data: T extends void | undefined | null ? T : Exclude<T, undefined>) => void> = [];
+export default class Relay<T = void> {
+    private onChangeCallbacks: Array<(data: T extends void ? void : T) => void> = [];
+    private promiseResolvers: Array<(data: T extends void ? void : T) => void> = [];
 
-    public addListener(callback: (data: T extends void | undefined | null ? T : Exclude<T, undefined>) => void): () => void {
+    public addListener(callback: (data: T extends void ? void : T) => void): () => void {
         this.onChangeCallbacks.push(callback);
         return () => this.removeListener(callback);
     }
 
-    public removeListener(callback: (data: T extends void | undefined | null ? T : Exclude<T, undefined>) => void): void {
+    public removeListener(callback: (data: T extends void ? void : T) => void): void {
         this.onChangeCallbacks = this.onChangeCallbacks.filter(cb => cb !== callback);
     }
 
-    public dispatch(data: T extends void | undefined | null ? T : Exclude<T, undefined>): void {
+    public dispatch(data?: T extends void ? never : T): void {
         for (let callback of this.onChangeCallbacks) {
-            callback(data);
+            callback(data as T extends void ? void : T);
         }
         while (this.promiseResolvers.length) {
             const resolver = this.promiseResolvers.pop();
             if (resolver) {
-                resolver(data);
+                resolver(data as T extends void ? void : T);
             }
         }
     }
@@ -28,28 +28,28 @@ export default class Relay<T> {
         this.promiseResolvers = [];
     }
 
-    public then(resolve: (value: T extends void | undefined | null ? T : Exclude<T, undefined>) => void, reject: (reason?: any) => void): void {
-        const newPromise = new Promise<T extends void | undefined | null ? T : Exclude<T, undefined>>((res, rej) => {
+    public then(resolve: (value: T extends void ? void : T) => void, reject: (reason?: any) => void): void {
+        const newPromise = new Promise<T extends void ? void : T>((res, rej) => {
             this.promiseResolvers.push(res);
         });
         newPromise.then(resolve, reject);
     }
 }
 
-type RelayFunction<T> = {
-    (): Promise<T extends void | undefined | null ? T : Exclude<T, undefined>>;
-    (callback: (data: T extends void | undefined | null ? T : Exclude<T, undefined>) => void): () => void;
+type RelayFunction<T = void> = {
+    (): Promise<T extends void ? void : T>;
+    (callback: (data: T extends void ? void : T) => void): () => void;
 }
 
-export function createRelay<T = undefined>(): Relay<T> & RelayFunction<T> {
+export function createRelay<T = void>(): Relay<T> & RelayFunction<T> {
     const relay = new Relay<T>();
 
-    const callableRelay = ((callback?: (data: T extends void | undefined | null ? T : Exclude<T, undefined>) => void) => {
+    const callableRelay = ((callback?: (data: T extends void ? void : T) => void) => {
         if (callback) {
             return relay.addListener(callback);
         } else {
-            return new Promise<T extends void | undefined | null ? T : Exclude<T, undefined>>((resolve) => {
-                const oneTimeListener = (data: T extends void | undefined | null ? T : Exclude<T, undefined>) => {
+            return new Promise<T extends void ? void : T>((resolve) => {
+                const oneTimeListener = (data: T extends void ? void : T) => {
                     resolve(data);
                     relay.removeListener(oneTimeListener);
                 };
